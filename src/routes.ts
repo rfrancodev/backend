542749973889d0908940741a95469ce7d3070cf3
rfrancodev <francorfs@icloud.com>
2024-07-29 21:03:03 -0300

    Deletar cliente e listar clientes OK

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import {FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply} from "fastify";
 import { CreateCustomerController } from "./controllers/CreateCustomerController";
+import { ListCustomerController } from "./controllers/ListCustomerController";
+import { DeleteCustomerController } from "./controllers/DeleteCustomerController";
 
 
 export async function routes(fastify:FastifyInstance, options: FastifyPluginOptions) {
@@ -8,9 +10,24 @@ export async function routes(fastify:FastifyInstance, options: FastifyPluginOpti
             return ({ok: true})
         })
 
+
     fastify.post('/customer', async (request: FastifyRequest, reply: FastifyReply) => {
       
         return new CreateCustomerController().handle(request, reply)
     })
+
+
+    fastify.get('/customers', async (request: FastifyRequest, reply: FastifyReply) => {
+      
+        return new ListCustomerController().handle(request, reply)
+    })
+    
+
+    fastify.delete('/customer', async (request: FastifyRequest, reply: FastifyReply) => {
+      
+        return new DeleteCustomerController().handle(request, reply)
+    })
+    
+   
 
 }
